d47ae1ec9c989bc8657f05d6edb5c0f4f5d760bf
Rosemary Joe <jjj430@msn.com>
2016-10-11 22:21:57 -0700

    add page routes to page.js

---
+++ js/routes.js
@@ -0,0 +1,7 @@
+page('/', mainController.index);
+page('/about', aboutController.index);
+page('/form', formController.index);
+page('/team', teamController.index);
+
+//  Activate page.js!
+page();
