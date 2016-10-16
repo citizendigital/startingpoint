7e0ba2b1386ad5fba96ac92bbce9ef7d2fc9a51f
Rosemary Joe <jjj430@msn.com>
2016-10-15 23:30:13 -0700

    add top5view.js and template

---
+++ js/top5View.js
@@ -0,0 +1,25 @@
+(function (module) {
+    var top5View = {};
+
+    var render = function (data) {
+        var top5Template = Handlebars.compile($('#top5Template').text());
+
+        return top5Template(data);
+    };
+
+    // tweetView.newest = function (data) {
+    //     $("#tweetsPoolPopular").fadeOut(300);
+    //     $('#tweetsPoolNew').append(
+    //         data.map(render)
+    //     ).delay(100).fadeIn(300);
+    // };
+
+    top5View.popular = function (data) {
+       // $("#tweetsTop5").fadeOut(300);
+        $('#tweetsTop5').append(
+            data.map(render)
+        ).delay(100).fadeIn(300);
+    };
+
+    module.top5View = top5View;
+})(window);
